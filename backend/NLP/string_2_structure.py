e9e9756d20c3d621dfc8274b8d444611eb5a5556
Vance Wood <48934755+vance5427@users.noreply.github.com>
2020-12-21 00:14:56 -0500

    some sql, fix small thing in nlp

--- backend/NLP/string_2_structure.py
+++ backend/NLP/string_2_structure.py
@@ -6,46 +6,70 @@ neg = ["not"]
 songs = ["Humble", "Hello", "Watermelon Sugar"]
 artists = ["Kendrick Lamar", "Adele", "Harry Styles"]
 years = ["2017", "2015", "2019"]
+decs = ["1990s", "2000s", "2010s"]
+genres = ["Rap", "Vocal", "Pop"]
 
 inp = "like watermelon sugar"
 
 # main function that takes in the user input and returns sql query for coorect info
 # length taken into account later
-def sqlFromClause(inp) :
+def query_from_clause(inp) :
     
     # get qualifier
     # returns tuple with the qualifier, then exc or inc depending on type of qualifier
-    qual = findQual(inp)
+    qual = find_qual(inp)
 
     # if no qualifier found, bad inp
     if (qual = 0)
         return 0
         
     # split string into before and after qualifier
-    splitString = inp.partition(qual[0])
+    split_string = inp.partition(qual[0])
         
     # check before qualifier if it's neg
-    n = isneg(splitString[0])
+    n = is_neg(splitString[0])
 
     # find the object 
-    obj = findObj(splitString[2])
-    
+    obj = find_obj(splitString[2])
 
+    # user can't ask for from or by a song
+    if ((qual[1] == "inc") and (obj[0] == "songs")):
+        return 0
 
-def findQual(inp):
-    in = inp.split()
-    for i in in:
+    return qual[1], n, obj[0], obj[1]
+
+def find_qual(inp):
+    str = inp.split()
+    for i in str:
         if i in exc:
             return i, exc
         else if i in inc:
             return i, inc
     return 0    
     
-def isneg(beg):
-    in = beg.split()
-    for i in in:
+def is_neg(beg):
+    str = beg.split()
+    for i in str:
         if i in neg:
             return 1
     return 0  
     
-def findObj(end):
+def find_obj(end):
+    str = end.split()
+    # check if a year or decade
+    for i in str:
+        if i in years:
+            return years, i
+        if i in decs:
+            return decs, i
+
+    # if not we need to check groups of words
+    if end in artists:
+        return artists, end
+    if end in songs:
+        return songs, end
+
+    return 0
+
+
+
