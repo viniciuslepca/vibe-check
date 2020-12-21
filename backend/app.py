af4bc5282720ec76df4f5a58b7f54d4b7915e461
Vinicius Lepca <vinicius.lepca@gmail.com>
2020-12-21 14:20:00 -0300

    Implement playlist generation API, finish frontend

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,17 @@
 import os
 
 import psycopg2
-from flask import Flask, jsonify, send_from_directory
+from flask import Flask, jsonify, send_from_directory, request
+from flask_cors import CORS
 
 app = Flask(__name__, static_folder='../../frontend/build')
+CORS(app)
 
 dbname = 'vibe-check'
 user = 'postgres'
 conn = psycopg2.connect(f"dbname={dbname} user={user}")
 cursor = conn.cursor()
+
 
 # Serve React App
 @app.route('/', defaults={'path': ''})
@@ -19,10 +22,14 @@ def serve(path):
     else:
         return send_from_directory(app.static_folder, 'index.html')
 
-@app.route('/')
-def index():
-    cursor.execute("SELECT * FROM song;")
-    return jsonify(cursor.fetchall())
+
+@app.route('/playlists', methods=['POST'])
+def generate_playlist():
+    body = request.get_json()
+    duration = body['duration']
+    queries = body['queries']
+
+    return jsonify("received")
 
 if __name__ == '__main__':
     app.run(debug=True)
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -21,6 +21,47 @@ class App extends React.Component {
 }
 
 class PlaylistInputs extends React.Component {
+    constructor(props) {
+        super(props);
+        const defaultQuery = "like Watermelon Sugar";
+        const defaultTimeMinutes = 30;
+        this.state = {
+            inputQueries: [defaultQuery],
+            defaultQuery: defaultQuery,
+            defaultTimeMinutes: defaultTimeMinutes,
+            timeInput: [0, defaultTimeMinutes] // [hours, minutes]
+        }
+    }
+
+    setInputQueries = (queries) => {
+        this.setState({inputQueries: queries});
+    }
+
+    resetInputQueries = () => {
+        this.setState({inputQueries: [this.state.defaultQuery]});
+    }
+
+    setTimeInput = (time) => {
+        this.setState({timeInput: time})
+    }
+
+    generatePlaylist = async () => {
+        const timeInput = this.state.timeInput;
+        const timeInSeconds = 3600 * timeInput[0] + 60 * timeInput[1];
+        const params = {
+            duration: timeInSeconds,
+            queries: this.state.inputQueries
+        }
+        const response = await fetch("http://localhost:5000/playlists", {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(params)
+        }).then(response => response.json());
+        console.log(response);
+    }
+
     render() {
         const leftWidth = 5;
         const rightWidth = 12 - leftWidth;
@@ -39,7 +80,10 @@ class PlaylistInputs extends React.Component {
                         <span style={whiteTextStyle}>Make a playlist with songs...</span>
                     </Col>
                     <Col xs={rightWidth}>
-                        <InputQueries/>
+                        <InputQueries setInputQueries={this.setInputQueries}
+                                      resetInputQueries={this.resetInputQueries}
+                                      inputQueries={this.state.inputQueries}
+                                      defaultQuery={this.state.defaultQuery}/>
                     </Col>
                 </Row>
                 <Row style={{marginBottom: standardMargin}}>
@@ -47,11 +91,13 @@ class PlaylistInputs extends React.Component {
                         <span style={whiteTextStyle}>I want my playlist to last</span>
                     </Col>
                     <Col xs={rightWidth}>
-                        <TimeQuery/>
+                        <TimeQuery timeInput={this.state.timeInput}
+                                   defaultTimeMinutes={this.state.defaultTimeMinutes}
+                                   setTimeInput={this.setTimeInput}/>
                     </Col>
                 </Row>
                 <Row style={{marginBottom: 20, marginRight: 20, float: "right"}}>
-                    <Button variant="primary">Generate Playlist!</Button>
+                    <Button variant="primary" onClick={this.generatePlaylist}>Generate Playlist!</Button>
                 </Row>
             </Container>
         );
@@ -59,10 +105,23 @@ class PlaylistInputs extends React.Component {
 }
 
 class TimeQuery extends React.Component {
+    onChange = (event) => {
+        const target = event.target;
+        const id = target.id;
+        const value = target.value;
+        const newTimeInput = this.props.timeInput;
+        if (id === "hoursQuery") {
+            newTimeInput[0] = value;
+        } else if (id === "minutesQuery") {
+            newTimeInput[1] = value;
+        }
+        this.props.setTimeInput(newTimeInput);
+    }
+
     render() {
         const fontSize = 22;
         return (
-            <Form>
+            <Form onChange={this.onChange} onSubmit={(event) => event.preventDefault()} >
                 <Form.Row>
                     <Col>
                         <Form.Group controlId={"hoursQuery"} style={{marginBottom: 0}}>
@@ -72,7 +131,7 @@ class TimeQuery extends React.Component {
                     </Col>
                     <Col>
                         <Form.Group controlId={"minutesQuery"} style={{marginBottom: 0}}>
-                            <Form.Control size="lg" type="number" min={0} defaultValue={30}/>
+                            <Form.Control size="lg" type="number" min={0} defaultValue={this.props.defaultTimeMinutes}/>
                         </Form.Group>
                         <span style={{fontSize: fontSize, float: "right"}}>minutes</span>
                     </Col>
@@ -86,7 +145,7 @@ class InputQueries extends React.Component {
     constructor(props) {
         super(props);
         const exampleQueries = [
-            "like Watermelon Sugar",
+            this.props.defaultQuery,
             "similar to Pink Floyd",
             "from the 80s",
             "by U2",
@@ -100,21 +159,37 @@ class InputQueries extends React.Component {
     }
 
     incrementNumQueries = () => {
-        this.setState({numQueries: this.state.numQueries + 1})
+        this.setState({numQueries: this.state.numQueries + 1});
+        const newInputQueries = this.props.inputQueries;
+        newInputQueries.push("");
+        this.props.setInputQueries(newInputQueries);
     }
 
     resetQueries = () => {
         this.setState({numQueries: 1})
+        const value = document.getElementById("query0").value;
+        this.props.setInputQueries([value]);
+    }
+
+    onBlur = (event) => {
+       const target = event.target;
+       if (target.tagName.toLowerCase() === "input") {
+           const value = target.value;
+           const index = target.id.split("query")[1]
+           const newInputQueries = this.props.inputQueries;
+           newInputQueries[index] = value;
+           this.props.setInputQueries(newInputQueries);
+       }
     }
 
     render() {
         return (
-            <Form>
+            <Form onBlur={this.onBlur} onSubmit={(event) => event.preventDefault()}>
                 {
                     [...Array(this.state.numQueries)].map((_, i) => {
                         return (
                             <Form.Group key={i} controlId={"query" + i}>
-                                <Form.Control size="lg" type="text"
+                                <Form.Control size="lg" type="text" required
                                               placeholder={this.state.exampleQueries[i % (this.state.exampleQueries.length)]}/>
                             </Form.Group>
                         );
